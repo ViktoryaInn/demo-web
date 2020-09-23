7f4b4aab71c84a81f0e5b6fe7c7e842501ffb68b
Губайдуллина Виктория <vicmurrmiu@gmail.com>
2020-09-23 21:19:42 +0300

    bootstrap

--- src/main/java/com/example/demoweb/model/Post.java
+++ src/main/java/com/example/demoweb/model/Post.java
@@ -1,11 +1,20 @@
 package com.example.demoweb.model;
 
+import java.util.Date;
+
 public class Post {
     private String text;
     private Integer likes = 0;
+    private Date creationDate;
 
     public Post(String text){
         this.text = text;
+        this.creationDate = new Date();
+    }
+
+    public Post(String text, Date creationDate){
+        this.text = text;
+        this.creationDate = creationDate;
     }
 
     public String getText() {
@@ -15,6 +24,8 @@ public class Post {
     public Integer getLikes() {
         return likes;
     }
+
+    public Date getCreationDate(){return creationDate;}
 
     public void setLikes(Integer likes) {
         this.likes = likes;
