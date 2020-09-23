442a2bc77ce84c3f58777898454e8350428d0b5c
Губайдуллина Виктория <vicmurrmiu@gmail.com>
2020-09-23 22:01:39 +0300

    create posts

--- src/main/java/com/example/demoweb/service/PostService.java
+++ src/main/java/com/example/demoweb/service/PostService.java
@@ -3,14 +3,23 @@ package com.example.demoweb.service;
 import com.example.demoweb.model.Post;
 import org.springframework.stereotype.Service;
 import java.util.ArrayList;
+import java.util.Date;
 
 @Service
 public class PostService {
-    public ArrayList<Post> listAllPosts(){
-        ArrayList<Post> posts = new ArrayList<Post>();
+    ArrayList<Post> posts = new ArrayList<Post>();
+
+    public PostService(){
         posts.add(new Post("MEOW"));
         posts.add(new Post("HA T'FU"));
         posts.add(new Post("PFFF"));
+    }
+
+    public ArrayList<Post> listAllPosts(){
         return posts;
+    }
+
+    public void create(String text) {
+        posts.add(new Post(text, new Date()));
     }
 }
