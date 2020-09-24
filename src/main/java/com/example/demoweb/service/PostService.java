cb96344750697b1bb60f4e53d624f6d9272783c2
Губайдуллина Виктория <vicmurrmiu@gmail.com>
2020-09-24 21:21:54 +0300

    patch

--- src/main/java/com/example/demoweb/service/PostService.java
+++ src/main/java/com/example/demoweb/service/PostService.java
@@ -1,25 +1,24 @@
 package com.example.demoweb.service;
 
 import com.example.demoweb.model.Post;
+import com.example.demoweb.repository.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.Date;
 
 @Service
 public class PostService {
-    ArrayList<Post> posts = new ArrayList<Post>();
 
-    public PostService(){
-        posts.add(new Post((long) posts.size(),"MEOW"));
-        posts.add(new Post((long) posts.size(), "HA T'FU"));
-        posts.add(new Post((long) posts.size(), "PFFF"));
-    }
+    @Autowired
+    PostRepository postRepository;
 
-    public ArrayList<Post> listAllPosts(){
-        return posts;
+    public Iterable<Post> listAllPosts() {
+        return postRepository.findAll();
     }
 
     public void create(String text) {
-        posts.add(new Post((long) posts.size(), text, new Date()));
+        Post post = new Post(null, text, new Date());
+        postRepository.save(post);
     }
 }
